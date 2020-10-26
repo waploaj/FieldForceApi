41b9eeadd6df0d371f5f498f941daa935a019300
Waploaj <amtz@AMTZs-MacBook-Pro.local>
2020-10-26 12:27:01 +0300

    update attendance  api

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object("Config.config")
--- attendance/attendance.py
+++ attendance/attendance.py
@@ -1,21 +1,34 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from .model import Attendance
 from app import db
-
+from app import create_app
+from werkzeug.exceptions import HTTPException
 
 
 attendance = Blueprint('attendace', __name__)
 
-@attendance.route('/', methods=["GET", "POST"])
-def save_employee_attendance(employee_data):
-    if request.methods == "POST":
-        em = Attendance(check_in = request.json['check_in'],
-                        check_out= request.json["check_out"],
-                        latitude = request.json["latitude"],
-                        longitude = request.json["longitude"],
-                        customer_id = request.json["customer_id"],
-                        employee_id = request.json["employee_id"])
-        db.session.add(em)
-        db.session.commit()
 
-    return "200"
+@attendance.route('/', methods=["GET", "POST"])
+def save_employee_attendance():
+
+    if request.method == "POST":
+        try:
+            em = Attendance(check_in = request.json['check_in'],
+                            check_out= request.json["check_out"],
+                            latitude = request.json["latitude"],
+                            longitude = request.json["longitude"],
+                            customer_id = request.json["customer_id"],
+                            employee_id = request.json["employee_id"])
+            if em:
+                db.session.add(em)
+                db.session.commit()
+            else:
+                return "Request  Body is missing"
+        except Exception as e:
+            pass
+
+    else:
+        return Response(status=405)
+
+
+    return Response(status=200)
