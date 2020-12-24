371821388dad55e93271106e0493d0f373241e76
Abdillah Galia <waploaj@icloud.com>
2020-12-24 12:54:18 +0300

    business logic goes here

--- Competitor/competitorproduct.py
+++ Competitor/competitorproduct.py
@@ -1,51 +1,12 @@
 from flask import Blueprint, request, jsonify
 from app import db
-from .model import Competitor
+from .model import *
 from Utily.auth import token_required
-from .serializer import CompetitorSerializer
+from .serializer import *
 
-comp = Blueprint("comp", __name__)
-
-sm = CompetitorSerializer()
+comp = Blueprint("comp", __name__ )
 
 
-@comp.route("/competior", methods=["GET", "POST"])
-def upload_competitor_data():
-    """
-
-    """
-    if request.method == "POST":
-        try:
-            competitor = Competitor(item_id = request.json["item_id"],
-                                    holding_stock = request.json["holding_stock"],
-                                    selling_stock = request.json["selling_stock"],
-                                    comments= request.json["comments"]
-
-            )
-            if sm.data_not_blank(competitor):
-                db.session.add(competitor)
-                db.session.commit()
-                print("okay")
-            else:
-                raise ValueError("Missing data")
-        except Exception as e:
-            pass
-        return jsonify(200)
-
-@comp.route("/competitor_all", methods=["GET, POST"])
-def get_competitor_data():
-    """
-
-    """
-    if request.method == "POST":
-        try:
-         result = Competitor.query.all()
-
-        except Exception:
-            pass
-        results = sm.dump(result, many=True)
-    return results
-
-
+#TODO: Add business logic Here.
 
 
