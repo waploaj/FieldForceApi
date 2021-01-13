a72d04422a54d57432b11a038386ecec0f859119
Abdillah Galia <waploaj@icloud.com>
2021-01-13 11:57:56 +0300

    update serializer/Todo add business logic

---
+++ CompetitorI/competitor.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from .model import Item
+from Utily.auth import token_required
+from .serializer import *
+
+comp = Blueprint("comp", __name__ )
+
+@comp.route("/env/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+       rm = Item(name = request.json["name"])
+    return jsonify(rm)
+
+#TODO: Add business logic Here.
---
+++ CompetitorI/serializer.py
@@ -0,0 +1,14 @@
+# from app import ma
+# from attendance.model import Attendance
+# from marshmallow import ValidationError
+#
+#
+# class AttendanceSerializer(ma.SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = Attendance
+#
+#     @staticmethod
+#     def data_not_blank(data):
+#         if not data:
+#             return ValidationError("Data not provide")
+#         return True
